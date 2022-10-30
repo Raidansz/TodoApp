e937b93017dac45bc7116308e0c92c27542829f1
Raidansz <92235772+Raidansz@users.noreply.github.com>
2022-10-30 23:53:01 +0100

    Geting the search bar to work

--- TodoApp/Controllers/TodoListViewController.swift
+++ TodoApp/Controllers/TodoListViewController.swift
@@ -136,7 +136,18 @@ extension TodoListViewController:UISearchBarDelegate{
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request:NSFetchRequest<Item> = Item.fetchRequest()
-        print(searchBar.text!)
+       
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text ?? "")
+        request.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+        
+        do{
+            itemArray =  try context.fetch(request)
+        }catch{
+            print(error.localizedDescription)
+        }
+        tableView.reloadData()
     }
     
     
